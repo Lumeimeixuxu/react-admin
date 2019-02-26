c87cc3b2361dea48b56d6fff1bfd4927c1015981
lumeixu <1842057688@qq.com>
2019-02-26 23:15:52 +0800

    '完成表单数据，静态页面，收集表单数据'

---
+++ src/components/login-form/index.jsx
@@ -0,0 +1,77 @@
+import React,{Component} from 'react';
+import {Form, Icon, Input, Button,message} from 'antd';
+const Item=Form.Item;
+class LoginForm extends Component{
+  checkPassword=(rules,value,callback)=>{
+    if(!value){
+      callback('请输入密码')
+    }else if(value.length<=4){
+      callback('密码长度不得小于4位')
+    }else if(value.length>=11){
+      callback('密码长度不得大于11位')
+    }else if(!(/^[a-zA-Z0-9_]+$/.test(value))){
+      callback('用户名必须为英文大小写字母，数字和下划线')
+    }else {
+    callback();
+    }
+  }
+  handleSubmit=e=>{
+    e.preventDefault();
+    const {validateFields,resetFields}=this.props.form;
+    validateFields((error,values)=>{
+      console.log(error, values);
+      if(!error){
+        console.log('收集的表单数据：', values);
+      }else{
+        //校验失败
+        //重置密码
+        resetFields(['password']);
+        const errMsg = Object.values(error).reduce((prev, curr) => prev + curr.errors[0].message + ' ', '')
+        message.error(errMsg);
+      }
+    })
+  }
+    render(){
+      const {getFieldDecorator}=this.props.form;
+      return(
+        <Form className='login-form-container' onSubmit={this.handleSubmit}>
+          <Item>
+            {
+              getFieldDecorator(
+                'username',
+                {
+                  rules: [
+                    { required: true, message: '请输入用户名!' },
+                    {min:4,message:'用户名必须大于4位'},
+                    {max:11,message:'用户名不能超过11位'},
+                    {pattern: /^[a-zA-Z0-9_]+$/,message:'用户名必须为英文大小写字母，数字和下划线'}
+                    ],
+                }
+              )( <Input placeholder='用户名'prefix={<Icon type="user" />} />)
+            }
+
+          </Item>
+          <Item>
+            {
+              getFieldDecorator(
+                'password',
+                {
+                  rules:[
+                    {validator:this.checkPassword}
+                  ]
+                }
+              )(
+                <Input  placeholder='密码'type='password' prefix={<Icon type="lock" />} />
+              )
+            }
+
+          </Item>
+          <Item>
+            <Button type='primary'htmlType="submit" className='login-form-button'>登录</Button>
+          </Item>
+        </Form>
+      )
+    }
+}
+const WrappedLoginForm=Form.create()(LoginForm);
+export default WrappedLoginForm;
